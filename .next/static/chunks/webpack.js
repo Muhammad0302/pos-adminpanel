5c9aa4ffb1d4ea1651ffcbaca2e2df7c3962d59c
@mi477048 <mi477048@gmail.com>
2024-01-24 12:55:06 -0300

    update: changes added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bc4e9e540f5e0290"; }
+/******/ 		__webpack_require__.h = function() { return "20ef047eaff33a6b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
