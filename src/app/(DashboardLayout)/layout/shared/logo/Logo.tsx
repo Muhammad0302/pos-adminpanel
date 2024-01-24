bb5fdc621ab7e3bc459517c5a4f938350e391444
@mi477048 <mi477048@gmail.com>
2024-01-24 17:50:45 -0300

    update: in progress changes

--- src/app/(DashboardLayout)/layout/shared/logo/Logo.tsx
+++ src/app/(DashboardLayout)/layout/shared/logo/Logo.tsx
@@ -1,20 +1,26 @@
-import Link from "next/link";
-import { styled } from "@mui/material";
-import Image from "next/image";
+import Link from 'next/link';
+import { styled } from '@mui/material';
+import Image from 'next/image';
 
 const LinkStyled = styled(Link)(() => ({
-  height: "70px",
-  width: "180px",
-  overflow: "hidden",
-  display: "block",
+	height: '70px',
+	width: '180px',
+	overflow: 'hidden',
+	display: 'block',
 }));
 
 const Logo = () => {
-  return (
-    <LinkStyled href="/">
-      <Image src="/images/logos/dark-logo.svg" alt="logo" height={70} width={174} priority />
-    </LinkStyled>
-  );
+	return (
+		<LinkStyled href="/" style={{ display: 'flex', justifyContent: 'center' }}>
+			<Image
+				src="/images/logos/logo.jpeg"
+				alt="logo"
+				height={70}
+				width={70}
+				priority
+			/>
+		</LinkStyled>
+	);
 };
 
 export default Logo;
